debbfb9f8dfd1b0398cdff34f5d3e5f18059ac2c
rooberttdev <robertbarbosa.dev@gmail.com>
2023-12-12 21:17:51 -0400

    variaveis parte 2

---
+++ logica-de-programação/variaveis2.dart
@@ -0,0 +1,20 @@
+void main (){
+  // valores mutaveis (ou seja, aceita alteração de valor)
+  // Tipo de dado + nome da variavel + valor da variavel
+  String curso = "Curso de Dart";
+
+  // Palavra reservar *var* + nome da variavel + valor da variavel
+  var idade = 29;
+
+  curso = "Curso de lógica de programação";
+  idade = 25;
+
+  // valores imutaveis (ou seja, não aceita alteração de valor)
+  //const = constante 
+  //final
+
+  const pi = 3.1416;
+
+
+
+}
