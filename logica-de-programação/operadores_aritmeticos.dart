46923e1e61a89a5deeaf4fc68e9e56fbbdd1828c
rooberttdev <robertbarbosa.dev@gmail.com>
2023-12-13 20:56:36 -0400

    Operadores aritmeticos

---
+++ logica-de-programação/operadores_aritmeticos.dart
@@ -0,0 +1,37 @@
+void main (){
+
+
+  //soma (+)
+  
+  final resultadoSoma = 10 + 5;
+
+  print("Soma: $resultadoSoma");
+
+  //subtração (-)
+
+  final resultadoSubtracao = 10 - 26;
+
+  print("Subtraçao: $resultadoSubtracao");
+
+  //multiplicação (*)
+  
+  final resultadoMultiplicacao = 10 * 5;
+
+  print("Multiplicação $resultadoMultiplicacao");
+
+  //divisão (/)
+
+  final resultadoDivisao = 10 / 5;
+
+  print("Divisão: $resultadoDivisao");
+
+  //resto da divisao (%)
+
+  final restoDivisao = 26 % 4;
+
+  print("Resto da divisão: $restoDivisao");
+
+
+
+
+}
