3ad8eca7a25dc839becf08751df5e59b00f2af99
rooberttdev <robertbarbosa.dev@gmail.com>
2023-12-13 21:48:30 -0400

    Estudando operadores relacionais

---
+++ logica-de-programação/operadores_relacionais.dart
@@ -0,0 +1,33 @@
+void main() {
+
+  final num1 = 10;
+  final num2 = 20;
+  final num3 = 10;
+
+  // == - igual
+  final eIgual = num1 == num2;
+  print("É igual: $eIgual");
+
+  // "!=" - diferente
+  final eDiferente = 10 != num2;
+  print("É diferente: $eDiferente");
+
+  // > - maior que
+  final eMaior = 20 > num3 ;
+  print("É maior: $eMaior");
+
+  // < - menor que
+  final eMenor = 20 < num3 ;
+  print("É menor: $eMenor");
+
+  // >= maior igual
+  final eMaiorOuIgual = 20 >= num2 ;
+  print("É maior ou igual: $eMaiorOuIgual");
+
+  // <= menor igual
+  final eMenorOuIgual = num1 <= num3 ;
+  print("É menor ou igual: $eMenorOuIgual");
+
+
+
+}
