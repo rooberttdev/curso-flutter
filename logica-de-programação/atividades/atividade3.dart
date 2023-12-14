ed908498e42b41f48e674a2b0b921a9b694f081c
rooberttdev <robertbarbosa.dev@gmail.com>
2023-12-13 21:32:07 -0400

    Resolução de exercicios de calculos

---
+++ logica-de-programação/atividades/atividade3.dart
@@ -0,0 +1,40 @@
+void main(){
+
+//Questão 1
+
+  final x = 14;
+  final y = 6;
+  final z = x + y;
+
+  print("O resultado da soma de $x e $y é $z.");
+
+//Questão 2
+
+  final totalCompra = 149.90;
+  final qtdParcelas = 2;
+  final valorParcela = 149.90 / 2;
+
+  print("O valor total da compra foi R\$ $totalCompra");
+  print("Forma de pagamento: ${qtdParcelas}x de R\$ $valorParcela");
+
+//Questão 3
+
+  int totalMinutos = 2;
+  int segundos = 60;
+  int totalEmSegundos = totalMinutos * segundos;
+
+  print("$totalMinutos minutos equivalem a $totalEmSegundos segundos!");
+
+//Questão 4
+
+  String nome = "Julianna";
+  int np1 = 10;
+  int np2 = 6;
+  int np3 = 5;
+  final media = (np1 + np2 + np3)/3;
+
+print("A aluna $nome ficou com média $media");
+
+
+
+}
