bfa796aededf8492f505bff010ff27d263119696
rooberttdev <robertbarbosa.dev@gmail.com>
2023-12-13 20:23:34 -0400

    concatenacao parte 1

---
+++ logica-de-programação/concatenacao.dart
@@ -0,0 +1,11 @@
+void main (){
+
+  final nome = "Robert";
+  final idade = 22;
+  final endereco = "Rua x, Cidade Y";
+
+  print("OLÁ, BOM DIA $nome, você tem $idade anos e mora no endereço: $endereco.");
+
+
+
+}
