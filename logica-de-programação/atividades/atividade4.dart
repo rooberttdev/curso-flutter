8ba5c0b55c92bda0ac940ef68c58ce5cf0f8ab16
rooberttdev <robertbarbosa.dev@gmail.com>
2023-12-13 23:35:31 -0400

    Resolução da atividade de if e else

---
+++ logica-de-programação/atividades/atividade4.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+void main(){
+  //Questão 1
+
+  stdout.write("Digite sua idade: ");  
+  final idade = double.parse(stdin.readLineSync()!);
+
+  if(idade > 18){
+
+    print("Você está apto a votar");
+  }else {
+    print("Você não tem idade para votar");
+  }
+  
+  //Questão 2
+  
+  stdout.write("Digite sua idade: ");  
+  final idade2 = double.parse(stdin.readLineSync()!);
+  stdout.write("Digite sua quantidade de dinheiro: ");  
+  final valor = double.parse(stdin.readLineSync()!);
+
+  if(idade2 > 21 && valor > 100){
+    print("Você pode entrar na festa!");
+
+  }else{
+    print("Você não cumpre com algum requisitos");
+  }
+
+  //Questão 3
+  
+  stdout.write("Digite um numero: ");  
+  final num1 = double.parse(stdin.readLineSync()!);
+  stdout.write("Digite outro numero: ");  
+  final num2 = double.parse(stdin.readLineSync()!);
+
+  if(num1==num2){
+    print("Números iguais!");
+  }if(num1 > num2){
+    print("O primeiro é maior!");
+  }if(num1 < num2){
+    print("O segundo é maior!");
+  }
+
+  //Questão 4
+  
+  stdout.write("Digite um numero: ");  
+  final num12 = double.parse(stdin.readLineSync()!);
+  final precoDuzia = 0.25;
+  final precoNaoDuzia = 0.30;
+  var resultado = num1;
+
+
+  if(num12 >= 12){
+    resultado = num1 * precoDuzia;
+    print("O valor da sua compre é: $resultado");
+  }if(num12 < 12){
+    resultado = num1 * precoNaoDuzia;
+    print("O valor da sua compre é: $resultado");
+  }
+  
+  
+  //Questão 5
+
+  stdout.write("Digite um numero: ");  
+  final num01 = double.parse(stdin.readLineSync()!);
+
+  if( num01 % 2 == 0){
+    print("Número é par!");
+  }else{
+    print("Número impar!");
+  }
+  
+}
